19094c1d435d54d1072ad4a54c41641ecd15a484
Brandon Conte <bconte950@gmail.com>
2022-04-05 18:44:18 -0400

    initial prompt setup with prompts for each option

--- index.js
+++ index.js
@@ -11,3 +11,62 @@ const employeePrompt = {
     viewAllRoles: "View All Roles",
     exit: "Exit"
 };
+// Enter password below for your mySQL connection
+const connection = mysql.createConnection({
+    host: 'localhost',
+    // Default port
+    port: 3306,
+    user: 'root',
+    password: 'PassHERE',
+    database: 'employee_db'
+});
+
+connection.connect(err => {
+    if (err) throw err;
+    prompt();
+});
+
+function prompt() {
+    inquirer
+        .prompt({
+            name: 'action',
+            type: 'list',
+            message: 'What would you like to do?',
+            choices: [
+                employeePrompt.viewAllEmployees,
+                employeePrompt.viewAllRoles,
+                employeePrompt.addEmployee,
+                employeePrompt.removeEmployee,
+                employeePrompt.updateRole,
+                employeePrompt.exit
+            ]
+        })
+        .then(answer => {
+            console.log('answer', answer);
+            switch (answer.action) {
+                case employeePrompt.viewAllEmployees:
+                    viewAllEmployees();
+                    break;
+
+                case employeePrompt.addEmployee:
+                    addEmployee();
+                    break;
+
+                case employeePrompt.removeEmployee:
+                    remove('delete');
+                    break;
+
+                case employeePrompt.updateRole:
+                    remove('roles');
+                    break;
+
+                case employeePrompt.viewAllRoles:
+                    viewAllRoles();
+                    break;
+
+                case employeePrompt.exit:
+                    connection.end();
+                    break;
+            }
+        });
+}
